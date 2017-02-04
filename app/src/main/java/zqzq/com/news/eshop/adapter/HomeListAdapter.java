3393b856f44cd4053ab1adf65463f38799532b1f
luwanmin <luwm805@163.com>
2017-02-04 19:39:59 +0800

    第一次提交

---
+++ app/src/main/java/zqzq/com/news/eshop/adapter/HomeListAdapter.java
@@ -0,0 +1,49 @@
+package zqzq.com.news.eshop.adapter;
+
+import android.content.Context;
+
+import java.util.List;
+
+import zqzq.com.news.eshop.R;
+
+/**
+ * Created by Administrator on 2017/2/4.
+ */
+
+public class HomeListAdapter extends BaseAdapter<Listben.DataEntity> {
+
+    /**
+     * @param context
+     * @param datas
+     * @param layoutIds :item 布局id 支持多种类型
+     */
+    public HomeListAdapter(Context context, List<Listben.DataEntity> datas, int[] layoutIds) {
+        super(context, datas, layoutIds);
+    }
+
+    @Override
+    public void setItemData(BaseViewHolder holder, Listben.DataEntity data, int itemType) {
+        holder.setText(R.id.tv_home_list_title,data.name);
+        switch (itemType){
+            case 0:
+                holder.setImagUrl(R.id.img_home_listitem01,data.goods.get(0).img.thumb);
+                holder.setImagUrl(R.id.img_home_listitem02,data.goods.get(1).img.thumb);
+                holder.setImagUrl(R.id.img_home_listitem03,data.goods.get(2).img.thumb);
+                break;
+            case 1:
+                holder.setImagUrl(R.id.img_home_listitem01,data.goods.get(0).img.thumb);
+                holder.setImagUrl(R.id.img_home_listitem02,data.goods.get(1).img.thumb);
+                holder.setImagUrl(R.id.img_home_listitem03,data.goods.get(2).img.thumb);
+                holder.setImagUrl(R.id.img_home_listitem04,data.goods.get(3).img.thumb);
+                break;
+            case 2:
+                holder.setImagUrl(R.id.img_home_listitem01,data.goods.get(0).img.thumb);
+                holder.setImagUrl(R.id.img_home_listitem02,data.goods.get(1).img.thumb);
+                holder.setImagUrl(R.id.img_home_listitem03,data.goods.get(2).img.thumb);
+                holder.setImagUrl(R.id.img_home_listitem04,data.goods.get(3).img.thumb);
+                holder.setImagUrl(R.id.img_home_listitem05,data.goods.get(4).img.thumb);
+                break;
+        }
+
+    }
+}
---
+++ app/src/main/java/zqzq/com/news/eshop/adapter/HomePrototAdapter.java
@@ -0,0 +1,8 @@
+package zqzq.com.news.eshop.adapter;
+
+/**
+ * Created by Administrator on 2017/2/4.
+ */
+
+public class HomePrototAdapter extends BaseAdapter<HeaderBean.DataEntity.PromoteGoodsEntity> {
+}
